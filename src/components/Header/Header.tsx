85053c5604c19827a85b00782ee7f36101c9c152
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2022-04-27 23:11:45 +0700

    Fix Logo to always be in the middle

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -11,18 +11,18 @@ export default function Header() {
     <NavBar>
       <HeaderContent>
         <Image src="/home.svg" alt="home" width="100%" height="100%"></Image>
+        <HeaderLogoWrapper>
+          <Image
+            src="/placeholder.png"
+            alt="icon"
+            width="240px"
+            height="60px"
+          ></Image>
+        </HeaderLogoWrapper>
+        <HeaderAddProductButtonWrapper>
+          <Image src="/add.svg" alt="add" width="35px" height="35px"></Image>
+        </HeaderAddProductButtonWrapper>
       </HeaderContent>
-      <HeaderLogoWrapper>
-        <Image
-          src="/placeholder.png"
-          alt="icon"
-          width="240px"
-          height="60px"
-        ></Image>
-      </HeaderLogoWrapper>
-      <HeaderAddProductButtonWrapper>
-        <Image src="/add.svg" alt="add" width="35px" height="35px"></Image>
-      </HeaderAddProductButtonWrapper>
     </NavBar>
   );
 }
