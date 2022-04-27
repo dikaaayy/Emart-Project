ab83bd76d52275c136997b64eb17f04f1a998354
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2022-04-27 14:52:22 +0700

    Product Page Finished

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,28 @@
+import {
+  HeaderAddProductButtonWrapper,
+  HeaderContent,
+  HeaderLogoWrapper,
+  NavBar,
+} from "../globalstyled";
+import Image from "next/image";
+
+export default function Header() {
+  return (
+    <NavBar>
+      <HeaderContent>
+        <Image src="/home.svg" alt="home" width="100%" height="100%"></Image>
+      </HeaderContent>
+      <HeaderLogoWrapper>
+        <Image
+          src="/placeholder.png"
+          alt="icon"
+          width="240px"
+          height="60px"
+        ></Image>
+      </HeaderLogoWrapper>
+      <HeaderAddProductButtonWrapper>
+        <Image src="/add.svg" alt="add" width="35px" height="35px"></Image>
+      </HeaderAddProductButtonWrapper>
+    </NavBar>
+  );
+}
