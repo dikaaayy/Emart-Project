db14fbf1416c37d60eac33f345e04c8c8348729d
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2022-04-28 20:27:26 +0700

    Resolved Merge issue

--- src/components/styled.ts
+++ src/components/styled.ts
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import styled from "styled-components";
 
 export const CardBox = styled.div`
@@ -5,3 +6,5 @@ export const CardBox = styled.div`
   height: 500px;
   width: 500px;
 `;
+=======
+>>>>>>> f4015a6 (Remove Import styled components)
