25611779d8c732b586d5ceea970623459b78c424
dikaaayy <andikayudhistira3@gmail.com>
2022-05-16 17:00:35 +0700

    adding responsive breakpoint in main page

--- src/components/ProductPage/styled.ts
+++ src/components/ProductPage/styled.ts
@@ -32,9 +32,18 @@ export const ProductName = styled.div`
 
 export const ProductGridDiv = styled.div`
   display: grid;
-  grid-template-columns: repeat(5, minmax(0, 1fr));
-  grid-column-gap: 35px;
-  grid-row-gap: 50px;
+  @media (min-width: 1024px) {
+    grid-template-columns: repeat(5, minmax(0, 1fr));
+    grid-column-gap: 35px;
+    grid-row-gap: 50px;
+  }
+  @media (min-width: 640px) {
+    grid-template-columns: repeat(3, minmax(0, 1fr));
+    grid-column-gap: 20px;
+    grid-row-gap: 30px;
+  }
+  grid-template-columns: repeat(2, minmax(0, 1fr));
+  grid-row-gap: 20px;
   justify-items: center;
   align-content: space-evenly;
   padding-top: 8rem;
