4b1b9c6733d0821bb8c850367a3706f26ab5627d
dikaaayy <andikayudhistira3@gmail.com>
2022-05-26 21:54:46 +0700

    api for searchproduct

--- pages/api/product/searchproduct.ts
+++ pages/api/product/searchproduct.ts
@@ -3,17 +3,17 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { prisma } from "../../../lib/prisma";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const { name, cost, description, productID } = req.body;
+  const { searchName } = req.body;
 
   try {
-    await prisma.product.create({
-      data: {
-        productID,
-        name,
-        description,
-        cost,
+    const response = await prisma.product.findMany({
+      where: {
+        name: {
+          contains: searchName,
+        },
       },
     });
+    res.send(response);
   } catch (e) {
     console.log(e);
   }
