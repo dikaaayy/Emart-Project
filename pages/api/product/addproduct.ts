11a2d07395257dbfdc1e7b1c698011ff8bf8ebaf
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2022-06-05 21:58:24 +0700

    Added Session on Api route product

--- pages/api/product/addproduct.ts
+++ pages/api/product/addproduct.ts
@@ -1,4 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 
 import { prisma } from "../../../lib/prisma";
 
@@ -8,22 +9,26 @@ export default async function handler(
 ) {
   const { name, cost, description, productID, imageUrl, stock, email } =
     req.body;
-
-  try {
-    await prisma.product.create({
-      data: {
-        productID,
-        name,
-        description,
-        cost,
-        imageUrl,
-        stock,
-        email,
-      },
-    });
-    res.status(200).end();
-  } catch (e) {
-    console.log(imageUrl);
-    console.log(e);
+  const session = await getSession({ req });
+  if (session) {
+    try {
+      await prisma.product.create({
+        data: {
+          productID,
+          name,
+          description,
+          cost,
+          imageUrl,
+          stock,
+          email,
+        },
+      });
+      res.status(200).end();
+    } catch (e) {
+      console.log(imageUrl);
+      console.log(e);
+    }
+  } else {
+    res.status(401);
   }
 }
--- pages/api/product/deleteproduct.ts
+++ pages/api/product/deleteproduct.ts
@@ -1,17 +1,26 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import { getSession } from "next-auth/react";
 
 import { prisma } from "../../../lib/prisma";
 
-export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const session = await getSession({ req });
   const ID: string = req.body;
-  try {
-    await prisma.product.delete({
-      where: {
-        productID: ID,
-      },
-    });
-    res.status(200).json({ message: "succesfuly updated" });
-  } catch (e) {
-    console.log(e);
+  if (session) {
+    try {
+      await prisma.product.delete({
+        where: {
+          productID: ID,
+        },
+      });
+      res.status(200).json({ message: "succesfuly updated" });
+    } catch (e) {
+      console.log(e);
+    }
+  } else {
+    res.status(401);
   }
 }
