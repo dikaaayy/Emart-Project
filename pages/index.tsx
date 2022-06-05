803c96ef04855250918f1459542d5d19427e37ab
Dwikavindra Haryo Radithya <dwikavindra.haryo.r@mail.ugm.ac.id>
2022-06-06 04:22:41 +0700

    Deleted Protected to Fix Protected

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,16 @@ import { useSession, getSession } from "next-auth/react";
 import Protected from "../src/components/Protected/Protected";
 import Head from "next/head";
 
-export async function getServerSideProps() {
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+  if (!session) {
+    return {
+      redirect: {
+        permanent: false,
+        destination: "/authPage/AuthenticationPage",
+      },
+    };
+  }
   const product: product[] = await prisma.product.findMany();
   return {
     props: {
@@ -21,14 +30,12 @@ type HomeProp = {
 export default function Home(props: HomeProp) {
   return (
     <>
-      <Protected>
-        <Head>
-          <title>Home | Emart</title>
-          <link rel="icon" href="/iconlogo.svg" />
-        </Head>
-        <Header />
-        <ProductPage products={props.products}></ProductPage>
-      </Protected>
+      <Head>
+        <title>Home | Emart</title>
+        <link rel="icon" href="/iconlogo.svg" />
+      </Head>
+      <Header />
+      <ProductPage products={props.products}></ProductPage>
     </>
   );
 }
